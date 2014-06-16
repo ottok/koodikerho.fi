7013129af5ec5aeb3bf56b33afda41d9cfb9185c
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2014-06-16 16:20:01 +0300

    Added jshintrc and fixed indentation etc

--- js/animation.js
+++ js/animation.js
@@ -1,29 +1,35 @@
-function animation() {
+/*jshint browser:true */
+/*global createjs, TweenLite, Quad */
+
+(function () {
+  'use strict';
+
+  function animation() {
 
     var colors = ['#d33', '#3d3', '#33d', '#dd3', '#3dd'];
-    var textStyle = "400 100px 'Open Sans',sans-serif";
+    var textStyle = '400 100px "Open Sans",sans-serif';
     var textString = 'KOODIKERHO.FI';
 
     var offsetX = (window.innerWidth-800)/2;
     var offsetY = (window.innerHeight-400)/2;
-    var textStage = new createjs.Stage("text");
-    textStage.canvas.width = innerWidth;
-    textStage.canvas.height = innerHeight;
+    var textStage = new createjs.Stage('text');
+    textStage.canvas.width = window.innerWidth;
+    textStage.canvas.height = window.innerHeight;
 
-    var stage = new createjs.Stage("stage");
+    var stage = new createjs.Stage('stage');
     stage.canvas.width = window.innerWidth;
     stage.canvas.height = window.innerHeight;
 
-    var text = new createjs.Text("t", textStyle, "#fff");
+    var text = new createjs.Text('t', textStyle, '#fff');
 
     // TODO: Adding dots on the fly would be cool,
     // but running simply addDot() does not weem to work..
-    function addDot() {
+    function addDot(count) {
       var circle = new createjs.Shape();
       var r = 8;
       var x = window.innerWidth*Math.random();
       var y = window.innerHeight*Math.random();
-      var color = colors[Math.floor(i%colors.length)];
+      var color = colors[Math.floor(count % colors.length)];
       var alpha = 0.2 + Math.random()*0.5;
       circle.alpha = alpha;
       circle.radius = r;
@@ -35,8 +41,8 @@ function animation() {
     }
 
     var circles = [];
-    for (var i = 1; i < 308; i++) {
-      addDot();
+    for (var count = 1; count < 308; count++) {
+      addDot(count);
     }
 
     text.text = textString;
@@ -49,13 +55,15 @@ function animation() {
 
     var ctx = document.getElementById('text').getContext('2d');
     var pix = ctx.getImageData(0,0,800,200).data;
-    textPixels = [];
+    var textPixels = [];
     for (var i = pix.length; i >= 0; i -= 4) {
-      if (pix[i] != 0) {
+      if (pix[i] !== 0) {
         var x = (i / 4) % 800;
-        var y = Math.floor(Math.floor(i/800)/4);
+        var y = Math.floor(Math.floor(i / 800) / 4);
 
-        if((x && x%8 == 0) && (y && y%8 == 0)) textPixels.push({x: x, y: y});
+        if((x && x % 8 === 0) && (y && y % 8 === 0)) {
+          textPixels.push({x: x, y: y});
+        }
       }
     }
     // console.log(textPixels.length);
@@ -65,7 +73,7 @@ function animation() {
       // if not enough dots, start distributing them randomly
       // to avoid big visible gaps in logo
       if (circles[k] === undefined) {
-        t = Math.floor(Math.random()*300);
+        var t = Math.floor(Math.random()*300);
         circles[t].originX = offsetX + textPixels[k].x;
         circles[t].originY = offsetY + textPixels[k].y;
         tweenCircle(circles[t]);
@@ -79,20 +87,20 @@ function animation() {
 
     // https://www.greensock.com/as/docs/tween/
     function tweenCircle(c) {
-    	c.tween = TweenLite.to(c, 3, {
-    	  x: c.originX,
-    	  y: c.originY,
-    	  ease:Quad.easeOut,
-    	  alpha: 1,
-    	  radius: 5,
-    	  scaleX: 0.4,
-    	  scaleY: 0.4,
-    	  onComplete: showSignupForm
-    	});
+      c.tween = TweenLite.to(c, 3, {
+        x: c.originX,
+        y: c.originY,
+        ease:Quad.easeOut,
+        alpha: 1,
+        radius: 5,
+        scaleX: 0.4,
+        scaleY: 0.4,
+        onComplete: showSignupForm
+      });
     }
 
     function showSignupForm(top, left) {
-		  document.getElementById('signup').style.opacity = 1;
+      document.getElementById('signup').style.opacity = 1;
     }
 
     // render a frame each 60 ms
@@ -103,8 +111,10 @@ function animation() {
     // stop rendering after 8 seconds
     setTimeout(function() {
       clearInterval(animationFrame);
-    }, 8000)
+    }, 8000);
 
-}
+  }
 
-window.onload = animation();
+  window.onload = animation();
+
+}());
